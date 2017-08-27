7baf7f5cc48bad0fb47eccc2ab0f468f7f0a0b6d
System <none>
2017-08-27 11:06:39 +0000

    Checkpoint

---
+++ dateManager.js
@@ -0,0 +1,66 @@
+var dateManager = module.exports = {
+   processDate(givenDate){
+    
+    // Converting date from a String to a Date format
+    var parsedDate = parseDate(givenDate);
+    
+    if (parsedDate) {
+      
+      var unixTime = toUnixTime(parsedDate);
+      var naturalTime = toNaturalTime(parsedDate);
+       
+      return( {'unix' : unixTime, 'natural': naturalTime} );
+      
+    } else {
+      
+      return( {'unix' : null, 'natural': null} );
+    
+    }   
+}
+};
+
+function parseDate(dateStr) {
+  
+  if (!isNaN(dateStr)) {
+    // Handle as unix epoch time
+    
+    // Converting our unix timestamp from seconds to millis (millis expected)
+    return new Date(parseInt(dateStr) * 1000);
+    
+  } else {
+    // Handle natural date string
+    
+    // Use Javascript's Date.parse to parse date strings in a number of forms
+    var millis = Date.parse(dateStr);
+    
+    if (isNaN(millis)) {
+      // If Date.parse fails, it returns NaN
+      return false;
+      
+    } else {
+      // Otherwise, a date was successfully identified and parsed
+      return new Date(millis);
+      
+    }
+  }
+  
+}
+
+const months = ["January", "February", "March", "April", "May", "June", "July", 
+"August", "September", "October", "November", "December"];
+
+// Converts a Date value to the equivalent UNIX epoch time (number).
+function toUnixTime(date) {
+  
+  // Date.getTime() returns epoch time in millis, so we divided to get seconds.
+  return (date.getTime() / 1000);
+  
+}
+
+// Converts a Date value to the equivalent natural language 
+function toNaturalTime(date) {
+  
+  return "" + date.getDate() + " " + months[date.getMonth()] + ", " 
+  + date.getFullYear();
+  
+}
--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var dateManager = require('dateManager');
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
@@ -39,84 +40,12 @@ app.route('/')
     })
 
 
-
 app.get('/:date', function(req, res) {
-    
-    // Retrieving date requested for conversion
-    var givenDate = req.params.date;
-    
-    // Converting date from a String to a Date format
-    var parsedDate = parseDate(givenDate);
-    
-    if (parsedDate) {
-      
-      var unixTime = toUnixTime(parsedDate);
-      var naturalTime = toNaturalTime(parsedDate);
-       
-      res.send( {'unix' : unixTime, 'natural': naturalTime} );
-      
-    } else {
-      
-      res.send( {'unix' : null, 'natural': null} );
-    
-    }
-    
-    res.end();
-    
+  res.send(dateManager(req.params.date));
+  res.end();
 });
 
 
-app.listen(8080, function () {
-  console.log('Timestamp service listening on port 8080!');
-});
-
-
-
-function parseDate(dateStr) {
-  
-  if (!isNaN(dateStr)) {
-    // Handle as unix epoch time
-    
-    // Converting our unix timestamp from seconds to millis (millis expected)
-    return new Date(parseInt(dateStr) * 1000);
-    
-  } else {
-    // Handle natural date string
-    
-    // Use Javascript's Date.parse to parse date strings in a number of forms
-    var millis = Date.parse(dateStr);
-    
-    if (isNaN(millis)) {
-      // If Date.parse fails, it returns NaN
-      return false;
-      
-    } else {
-      // Otherwise, a date was successfully identified and parsed
-      return new Date(millis);
-      
-    }
-  }
-  
-}
-
-const months = ["January", "February", "March", "April", "May", "June", "July", 
-"August", "September", "October", "November", "December"];
-
-// Converts a Date value to the equivalent UNIX epoch time (number).
-function toUnixTime(date) {
-  
-  // Date.getTime() returns epoch time in millis, so we divided to get seconds.
-  return (date.getTime() / 1000);
-  
-}
-
-// Converts a Date value to the equivalent natural language 
-function toNaturalTime(date) {
-  
-  return "" + date.getDate() + " " + months[date.getMonth()] + ", " 
-  + date.getFullYear();
-  
-}
 // Respond not found to all the wrong routes
 app.use(function(req, res, next){
   res.status(404);
